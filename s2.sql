88605b27b4645f05db3cc4c3fe1d90fb00f215cc
oracle <oracle@apexdb.(none)>
2017-04-04 14:49:52 +0300

    first commit

---
+++ s2.sql
@@ -0,0 +1,3 @@
+alter database tempfile '/u01/app/oracle/oradata/orcl/temp01.dbf' drop including datafiles;
+startup force nomount;
+exit;
---
+++ standbay.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+### set orahome ###
+# Oracle
+ORACLE_BASE=/u01/app/oracle; export ORACLE_BASE
+ORACLE_HOME=$ORACLE_BASE/product/11.2.0/dbhome_1; export ORACLE_HOME
+ORACLE_SID=orcl; export ORACLE_SID
+
+
+PATH=$PATH:$HOME/bin; export PATH
+PATH=$PATH:$ORACLE_HOME/bin; export PATH
+
+export NLS_LANG=AMERICAN_AMERICA.AL32UTF8
+
+cd /home/oracle/standbay
+
+rman  target / @s1.rman;
+sqlplus / as sysdba @s2.sql;
+rman target sys/AvhrSt31415926@master auxiliary / @s3.rman;
